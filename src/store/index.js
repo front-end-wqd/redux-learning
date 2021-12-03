422aee7cb646aa4c349c8fa650fef117cca8ed3b
wqd182212 <10104911+wqd182212@user.noreply.gitee.com>
2021-12-03 17:28:16 +0800

    添加中间件thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducer';
-
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+import thunk from 'redux-thunk';
+// 增强函数
+const composeEnhance = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+const store = createStore(reducer, composeEnhance(applyMiddleware(thunk)));
 
 export default store;
